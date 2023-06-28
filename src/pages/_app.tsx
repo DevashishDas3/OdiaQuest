cc5c246714873a0230d4fee50e4914335f4ed08d
DevashishDas3 <dedas2024@gmail.com>
2023-06-28 14:48:49 -0400

    v1.0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,35 @@
 import { type AppType } from "next/app";
+
 import { api } from "~/utils/api";
+
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
+
+/*
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+
+import { ClerkProvider } from  "@clerk/nextjs";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
+*/
